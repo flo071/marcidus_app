6d42180c008a4d34dd2eb40380bf64e6ff51dc20
flo071 <flo.hintermeier@gmail.com>
2017-03-01 17:43:37 +0100

    redo all

---
+++ app/src/main/java/marcidus/flo071/MainActivity.java
@@ -0,0 +1,22 @@
+package marcidus.flo071;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        mWebView.loadUrl("https://flo071.github.io/marcidus/");
+    }
+}
